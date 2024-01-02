1819c09f9910d4fac130ca14f417eb01f91ac939
IMTREDU <imtr.edu@gmail.com>
2024-01-02 16:41:20 +0000

    task 0-12

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    for char in s:
+        if ord('a') <= ord(char) <= ord('z'):
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+        else:
+            uppercase_char = char
+        print("{}".format(uppercase_char), end="")
+        print()
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
