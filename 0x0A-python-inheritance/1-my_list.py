94dec55d0874a91864e28edde0c39ec65b6649f6
IMTREDU <imtr.edu@gmail.com>
2024-02-05 19:07:58 +0000

    fIX

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module defining the MyList class."""
+"""Module for MyList class."""
+
 
 class MyList(list):
-    """Custom MyList class that extends the built-in list class."""
-
+    """Custom MyList class."""
     def print_sorted(self):
-        """Prints the elements of the list in sorted order."""
-        print(sorted_list)
+        """Method for printing sorted list."""
+        print(sorted(self))
