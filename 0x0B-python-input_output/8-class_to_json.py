e3eeae2895e60ffc1959786fb4a3d50689d9665c
IMTREDU <imtr.edu@gmail.com>
2024-02-06 21:47:29 +0000

    Fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,9 +3,10 @@
 Module to convert an object of a class to a dictionary for JSON serialization.
 """
 
+
+
 def class_to_json(obj):
     """
-    Convert an object of a class to a dictionary for JSON serialization.
-    
+    Convert an object of a class to a dictionary for JSON serialization. 
     """
     return obj.__dict__
