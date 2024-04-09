c1c7aa2fdab0b36a868588b67db90d0078a9fbd0
IMTREDU <imtr.edu@gmail.com>
2024-04-09 22:19:47 +0000

    Fix?

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,34 +1,32 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        } else {
-            Object.create(null);
-        }
+  constructor (width, height) {
+    if ((width > 0) && (height > 0)) {
+      this.width = width;
+      this.height = height;
     }
+  }
 
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            let row = '';
-            for (let j = 0; j < this.width; j++) {
-                row += 'X';
-            }
-            console.log(row);
-        }
+  print () {
+    for (let row = 0; row < this.height; row++) {
+      let line = '';
+      for (let col = 0; col < this.width; col++) {
+        line += 'X';
+      }
+      console.log(line);
     }
+  }
 
-    rotate() {
-        const temp = this.width;
-        this.width = this.height;
-        this.height = temp;
-    }
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
 
-    double() {
-        this.width *= 2;
-        this.height *= 2;
-    }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
