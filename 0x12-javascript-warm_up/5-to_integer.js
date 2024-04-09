ad99a69829b96d9c12d9fd934ba07b53668b1ea1
IMTREDU <imtr.edu@gmail.com>
2024-04-09 19:41:14 +0000

    Fix?

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-
-const num = parseInt(process.argv[2]);
-
-if (!isNaN(num)) {
-    console.log('My number:', num);
+const number = parseInt(process.argv[2]);
+if (isNaN(number)) {
+  console.log('Not a number');
 } else {
-    console.log('Not a number');
+  console.log('My number:', number);
 }
