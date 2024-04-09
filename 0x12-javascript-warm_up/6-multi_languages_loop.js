75a4a4496b78608f29190e2a64cf2dc1616d30f4
IMTREDU <imtr.edu@gmail.com>
2024-04-09 19:44:48 +0000

    Fix?

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-let output = '';
 for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-  output += line + '\n';
+	console.log(line);
 }
-console.log(output);
