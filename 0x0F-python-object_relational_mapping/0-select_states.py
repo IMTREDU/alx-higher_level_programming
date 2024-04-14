d0d20aae5e3c24b5bce339c238c5091f68b6506b
IMTREDU <imtr.edu@gmail.com>
2024-04-14 13:30:40 +0000

    Fix?

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 """
-Script that lists all states from the database hbtn_0e_0_usa,
-filtering out duplicates based on the id column.
+Script that lists all states from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    c = db.cursor()
-    c.execute("SELECT * FROM states WHERE id IN (SELECT MIN(id) FROM states GROUP BY name) ORDER BY id ASC")
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
-    c.close()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
     db.close()
