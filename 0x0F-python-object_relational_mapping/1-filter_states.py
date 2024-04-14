1e20105f2c9be6d4e7147e134eeb09a98a56743e
IMTREDU <imtr.edu@gmail.com>
2024-04-14 13:34:15 +0000

    Fix2?

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """
-This script retrieves and displays unique states from the database hbtn_0e_0_usa whose names begin with an uppercase 'N'.
+Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+
     cursor = db.cursor()
 
-    cursor.execute("""SELECT DISTINCT * FROM states WHERE name
-                      LIKE BINARY 'N%' AND id IN 
-                      (SELECT MIN(id) FROM states WHERE name LIKE BINARY 'N%' GROUP BY name)
-                      ORDER BY id""")
-    rows = cursor.fetchall()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    for row in rows:
-        print(row)
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
 
     cursor.close()
     db.close()
