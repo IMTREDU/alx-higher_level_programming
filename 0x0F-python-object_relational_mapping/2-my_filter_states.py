36072447afdda3acf8e894daa822f78b1b20f025
IMTREDU <imtr.edu@gmail.com>
2024-04-14 13:42:04 +0000

    fIX?

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
+Script that filters states by user input.
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
-    cursor = db.cursor()
-
-    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
-    cursor.execute(query, (sys.argv[4],))
-
-    rows = cursor.fetchall()
-
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+              .format(sys.argv[4]))
+    rows = c.fetchall()
     for row in rows:
         print(row)
-
-    cursor.close()
+    c.close()
     db.close()
