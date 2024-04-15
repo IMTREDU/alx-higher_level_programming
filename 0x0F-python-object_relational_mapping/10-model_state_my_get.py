d924b0fddc2128ed5a70516dc6834aa905ea06c7
IMTREDU <imtr.edu@gmail.com>
2024-04-15 12:27:51 +0000

    Fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-"""
-This script retrieves a State object from the database hbtn_0e_6_usa based on the state name passed as an argument.
-"""
+"""Script that prints the State object with the name passed as argument"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    import sys
-    from model_state import Base, State
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database_name state_name".format(
+            sys.argv[0]))
+        sys.exit(1)
 
-    inp = sys.argv
-    if len(inp) < 5 or ";" in inp[4]:
-        exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    conn_str = "mysql+mysqldb://{}:{}@localhost:3306/{}"
-    engine = create_engine(conn_str.format(inp[1], inp[2], inp[3]))
-    Session = sessionmaker(engine)
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database_name),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    my_query = session.query(State).filter(State.name.like(inp[4])).all()
-    if len(my_query) == 0:
-        print("Not found")
-    else:
-        print(my_query[0].id)
+    state = session.query(State).filter(State.name == state_name).first()
 
-    session.close()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
