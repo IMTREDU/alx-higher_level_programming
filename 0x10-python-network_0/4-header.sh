24b8a9abe28a8d7e9634f1054b2c95c3dfe6d54a
IMTREDU <imtr.edu@gmail.com>
2024-05-01 22:00:46 +0000

    Fix

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header to a URL
-curl -s -X GET -H "X-School-User-Id: 98" "$1" -o /dev/null -w "%{http_code}"
+# Sends a GET request to a URL and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
