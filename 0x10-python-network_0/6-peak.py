47d95f2cd253a13fe10ddb3bee6675bd7ceb9b07
IMTREDU <imtr.edu@gmail.com>
2024-05-01 21:45:47 +0000

    Fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
 """
-This script finds a peak in a list of unsorted integers
+This script finds a peak in a list of unsorted integers.
 """
 
-def find_peak(integers):
-
-    if integers is None or len(integers) == 0:
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
         return None
-
-    if len(integers) == 1:
-        return integers[0]
-
-    middle_index = int(len(integers) / 2)
-
-    if middle_index != len(integers) - 1:
-        if integers[middle_index - 1] < integers[middle_index] and\
-           integers[middle_index + 1] < integers[middle_index]:
-            return integers[middle_index]
-    else:
-        if integers[middle_index - 1] < integers[middle_index]:
-            return integers[middle_index]
+    low = 0
+    high = len(list_of_integers) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
         else:
-            return integers[middle_index - 1]
-
-    if integers[middle_index - 1] > integers[middle_index]:
-        sublist = integers[0:middle_index]
-    else:
-        sublist = integers[middle_index + 1:]
-
-    return find_peak(sublist)
-
+            low = mid + 1
+    return list_of_integers[low]
